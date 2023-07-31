13c3276f2e9dbe277945f971c7a5690130e41c23
qukjfraser <102866978+qukjfraser@users.noreply.github.com>
2023-07-31 16:18:32 +0100

    confirm changes

--- DotNetCoreSqlDb/Program.cs
+++ DotNetCoreSqlDb/Program.cs
@@ -7,11 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add database context and cache
 builder.Services.AddDbContext<MyDatabaseContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("AZURE_SQL_CONNECTIONSTRING")));
-builder.Services.AddStackExchangeRedisCache(options =>
-{
-    options.Configuration = builder.Configuration.GetConnectionString("AZURE_REDIS_CONNECTIONSTRING");
-    options.InstanceName = "SampleInstance";
-});
+
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
